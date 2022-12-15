6e13d54b9915ad6b9dcca687f1d9dc3d293a5f5d
César Augusto <ces.augusto108@gmail.com>
2022-12-15 19:05:06 -0300

    Rest template to communicate between hr-employee and hr-payroll to get an employee by id

---
+++ hr-payroll/src/main/java/ces/augusto108/hrpayroll/model/entities/Employee.java
@@ -0,0 +1,63 @@
+package ces.augusto108.hrpayroll.model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee implements Serializable {
+    private static final long serialVersionUID = -6724319448416134253L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name + " ( " + id + " )";
+    }
+}
