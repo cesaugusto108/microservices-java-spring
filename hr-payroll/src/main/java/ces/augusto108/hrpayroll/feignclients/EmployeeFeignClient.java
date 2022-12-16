1d9b469acc7ff1c15acc1faf085f559197f8b439
César Augusto <ces.augusto108@gmail.com>
2022-12-15 21:02:52 -0300

    Refactored code to use feign client to communicate between hr-employee and hr-payroll to get an employee by id

---
+++ hr-payroll/src/main/java/ces/augusto108/hrpayroll/feignclients/EmployeeFeignClient.java
@@ -0,0 +1,15 @@
+package ces.augusto108.hrpayroll.feignclients;
+
+import ces.augusto108.hrpayroll.model.entities.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-employee", url = "localhost:8001", path = "/employees")
+public interface EmployeeFeignClient {
+    @GetMapping(value = "/list/{id}")
+    ResponseEntity<Employee> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/ces/augusto108/hrpayroll/model/services/PaymentService.java
+++ hr-payroll/src/main/java/ces/augusto108/hrpayroll/model/services/PaymentService.java
@@ -1,35 +1,20 @@
 package ces.augusto108.hrpayroll.model.services;
 
+import ces.augusto108.hrpayroll.feignclients.EmployeeFeignClient;
 import ces.augusto108.hrpayroll.model.entities.Employee;
 import ces.augusto108.hrpayroll.model.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
-    @Value("${hr-employee.host}")
-    private String host;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private EmployeeFeignClient employeeFeignClient;
 
     public Payment getPayment(Long employeeId, Integer numberOfDays) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(employeeId));
+        Employee e = employeeFeignClient.findById(employeeId).getBody();
 
-        Employee e = restTemplate.getForObject(
-                host + "/employees/list/{id}",
-                Employee.class,
-                uriVariables
-        );
-
-        if (e != null) {
-            return new Payment(e.getName(), e.getDailyIncome(), numberOfDays);
-        } else return null;
+        if (e != null) return new Payment(e.getName(), e.getDailyIncome(), numberOfDays);
+        else return null;
     }
 }
