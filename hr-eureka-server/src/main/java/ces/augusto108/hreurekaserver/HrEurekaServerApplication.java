f458d85adfe65f8275defdaed751b364a95d13f0
César Augusto <ces.augusto108@gmail.com>
2022-12-15 23:43:45 -0300

    hr-eureka-server project created

---
+++ hr-eureka-server/src/main/java/ces/augusto108/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+}
---
+++ hr-eureka-server/src/test/java/ces/augusto108/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
