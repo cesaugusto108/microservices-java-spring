7375fee05b3d948548bc87b2484d042d9416a8c5
César Augusto <ces.augusto108@gmail.com>
2022-12-16 10:00:44 -0300

    Eureka server configured

--- hr-eureka-server/src/main/java/ces/augusto108/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/ces/augusto108/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,9 @@ package ces.augusto108.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 	public static void main(String[] args) {
