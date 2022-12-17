1d236e29487e76390d4eae6f2c113bca6de56d06
César Augusto <ces.augusto108@gmail.com>
2022-12-17 15:26:01 -0300

    hr-config-server module configuration

--- hr-config-server/src/main/java/ces/augusto108/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/ces/augusto108/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,9 @@ package ces.augusto108.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 	public static void main(String[] args) {
