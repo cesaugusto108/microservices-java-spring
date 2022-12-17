515e50757fe4e336b3cb39183c7cebc8ecf2ab05
César Augusto <ces.augusto108@gmail.com>
2022-12-17 16:00:35 -0300

    hr-employee config-server client configuration

--- hr-employee/src/main/java/ces/augusto108/hremployee/model/controllers/EmployeeController.java
+++ hr-employee/src/main/java/ces/augusto108/hremployee/model/controllers/EmployeeController.java
@@ -5,6 +5,7 @@ import ces.augusto108.hremployee.model.services.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,9 @@ import java.util.List;
 @RequestMapping(value = "/employees")
 public class EmployeeController {
     private final static Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private Environment environment;
@@ -42,6 +46,13 @@ public class EmployeeController {
     @DeleteMapping(value = "/delete/{id}")
     public void deleteById(@PathVariable Long id) {
         employeeService.deleteById(id);
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        LOGGER.info("Config: " + testConfig);
+
+        return ResponseEntity.noContent().build();
     }
 }
 
