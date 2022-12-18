ed57519b3ed741b8e78716523f6b5d39e693eb1c
César Augusto <ces.augusto108@gmail.com>
2022-12-18 16:46:21 -0300

    hr-oauth Eureka client configured

--- hr-oauth/src/main/java/ces/augusto108/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/ces/augusto108/hroauth/HrOauthApplication.java
@@ -2,7 +2,9 @@ package ces.augusto108.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 	public static void main(String[] args) {
